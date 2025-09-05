7be99975e9d683f875ff5270c5daec9174e4dec5
Raj singh <dynemaster8@gmail.com>
2025-09-05 18:09:59 +0530

    custom react is added

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,10 +1,11 @@
 import Youtube from "./Youtube";
 
 function App() {
+  const username = "Raj";
   return (
     <>
-      <h1> Vite react app</h1>
-      <h1> Vite react app</h1>
+      <h1> Vite react app {5 * 6}</h1>
+      <h1> Vite react app {username}</h1>
       <Youtube />
     </>
   );
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,4 +3,33 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
+const reactElement = {
+  type: "a", //tag type
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "click me to visit google",
+};
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React app</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="https://google.com" target="_blanck">
+    Visit Google
+  </a>
+);
+
+const areactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click to visit google"
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
